5bd01d523ccea88e77284cc2ac548da04d6ca2d1
Anish Kumar <anishk000kumar@gmail.com>
2024-04-02 21:06:39 +0530

    backup

--- database/seeders/SettingsTableSeeder.php
+++ database/seeders/SettingsTableSeeder.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
@@ -18,7 +19,7 @@ class SettingsTableSeeder extends Seeder
         $data = [
             ['type' => 'current_session', 'description' => '2018-2019'],
             ['type' => 'system_title', 'description' => 'CJIA'],
-            ['type' => 'system_name', 'description' => 'CJ INSPIRED ACADEMY'],
+            ['type' => 'system_name', 'description' => 'GPS School'],
             ['type' => 'term_ends', 'description' => '7/10/2018'],
             ['type' => 'term_begins', 'description' => '7/10/2018'],
             ['type' => 'phone', 'description' => '0123456789'],
@@ -38,6 +39,5 @@ class SettingsTableSeeder extends Seeder
         ];
 
         DB::table('settings')->insert($data);
-
     }
 }
